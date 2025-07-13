68179160b9f9a8ca614b7816c3cea72f22a6e89e
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-07-12 23:35:44 -0500

    added GET method with pagination to the record API

--- src/app/api/record/route.js
+++ src/app/api/record/route.js
@@ -2,8 +2,112 @@ import { db } from '@/db/drizzle'
 import { record } from '@/db/schema'
 import { auth } from '@/libs/auth'
 import { generateId } from '@/utils/generate-id'
+import { and, count, desc, eq } from 'drizzle-orm'
 import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
+
+export async function GET (req) {
+  try {
+    const data = await auth.api.getSession({
+      headers: await headers()
+    })
+    const user = data?.user
+
+    if (!user) {
+      return NextResponse.json({
+        success: false,
+        status_code: 401,
+        message: 'User is not authenticated'
+      }, { status: 401 })
+    }
+    const { searchParams, origin } = new URL(req.url)
+    const page = parseInt(searchParams.get('page')) || 1
+    const limit = parseInt(searchParams.get('limit')) || 10
+    const offset = (page - 1) * limit
+
+    const pickFields = {
+      id: record.id,
+      title: record.title,
+      content: record.content,
+      last_opened_at: record.lastOpenedAt,
+      created_at: record.createdAt,
+      updated_at: record.updatedAt
+    }
+
+    const [records, total] = await Promise.all([
+      db.select(pickFields)
+        .from(record)
+        .where(and(
+          eq(record.userId, user.id),
+          eq(record.favorite, false)
+        ))
+        .orderBy(desc(record.createdAt))
+        .limit(limit)
+        .offset(offset),
+      db.select({ count: count() })
+        .from(record)
+        .where(and(
+          eq(record.userId, user.id),
+          eq(record.favorite, false)
+        ))
+    ])
+    const baseUrl = `${origin}/api/record`
+    const totalPages = Math.ceil(total[0].count / limit)
+    const buildLink = (targetPage) => `${baseUrl}?page=${targetPage}&limit=${limit}`
+
+    if (records.length <= 0) {
+      return NextResponse.json({
+        success: true,
+        status_code: 200,
+        message: 'The records list is empty',
+        results: {
+          total_records: total[0].count,
+          pagination: {
+            total_pages: page,
+            current_page: limit,
+            per_page: 0
+          },
+          links: {
+            base_url: baseUrl,
+            first_url: buildLink(1),
+            prev_url: null,
+            next_url: null,
+            last_url: null
+          },
+          data: []
+        }
+      }, { status: 200 })
+    }
+
+    return NextResponse.json({
+      success: true,
+      status_code: 200,
+      message: 'List of records',
+      results: {
+        total_records: total[0].count,
+        pagination: {
+          total_pages: Math.ceil(total[0].count / limit),
+          current_page: page,
+          per_page: limit
+        },
+        links: {
+          base_url: baseUrl,
+          first_url: buildLink(1),
+          prev_url: page > 1 ? buildLink(page - 1) : null,
+          next_url: page < totalPages ? buildLink(page + 1) : null,
+          last_url: buildLink(totalPages)
+        },
+        data: records
+      }
+    }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      status_code: 500,
+      message: error?.message || 'Unexpected server error'
+    }, { status: 500 })
+  }
+}
 
 export async function POST (req) {
   try {
@@ -24,7 +128,7 @@ export async function POST (req) {
     const pickFields = {
       id: record.id,
       title: record.title,
-      createdAt: record.createdAt
+      created_at: record.createdAt
     }
     const nanoid = generateId()
 
